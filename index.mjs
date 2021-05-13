b943b172533236efd63ac483c49ba9672799129c
Robin Drexler <drexler.robin@gmail.com>
2021-05-13 19:45:11 -0400

    add sourcemaps support

--- index.mjs
+++ index.mjs
@@ -1,5 +1,11 @@
 import { buildSync } from "esbuild";
 import Module from "module";
+import sourceMapSupport from "source-map-support";
+
+sourceMapSupport.install({
+  environment: "node",
+  hookRequire: true,
+});
 
 /**
  * @typedef {Object} Options
@@ -10,16 +16,22 @@ import Module from "module";
  *
  * @param options {Options}
  */
-export async function run({ filePath } = {}) {
+export function run({ filePath } = {}) {
   Module._extensions[".ts"] = (module, filename) => {
     const result = buildSync({
       entryPoints: [filename],
       write: false,
       platform: "node",
       format: "cjs",
+      sourcemap: "inline",
     });
 
-    module._compile(result.outputFiles[0].text, filename);
+    const originalCompile = module._compile;
+    module._compile = function (module, filename) {
+      return originalCompile.call(this, result.outputFiles[0].text, filename);
+    };
+
+    return Module._extensions[".js"](module, filename);
   };
 
   Module.runMain(filePath);
